56d5e28a53ac374c5c27f73f8b6576057b7b3215
bitbckt <brandon@systemisdown.net>
2010-03-04 11:08:14 -0800

    Initial implementation.

---
+++ lib/resque/lifecycle.rb
@@ -0,0 +1,29 @@
+module Resque
+
+  module Lifecycle
+
+    def self.included(base) #:nodoc:
+      base.class_eval do
+        alias_method :push_without_lifecycle, :push
+        extend ClassMethods
+      end
+    end
+
+    module ClassMethods
+
+      # Wrapper for the original Resque push method, which adds a
+      # created_at timestamp to any +item+ pushed onto the queue which
+      # responds to #[]=.
+      #
+      def push(queue, item)
+        if item.respond_to?(:[]=)
+          item[:created_at] = Time.now.to_i
+        end
+        push_without_lifecycle queue, item
+      end
+
+    end
+
+  end
+
+end
