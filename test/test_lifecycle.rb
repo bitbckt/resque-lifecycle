e62388dce1138d08eacdcf78fe2636a130640fb7
Matt Conway <wr0ngway@yahoo.com>
2011-02-03 12:14:30 -0500

    symbols get converted into strings when converting to json for push, so check for prescence of created_at with a string key

--- test/test_lifecycle.rb
+++ test/test_lifecycle.rb
@@ -11,14 +11,14 @@ class LifecycleTest < Test::Unit::TestCase
   end
 
   should 'add created_at to items with #[]=' do
-    assert_not_nil ResqueMock.push('queue', { })[:created_at]
+    assert_not_nil ResqueMock.push('queue', { })['created_at']
   end
 
   should 'not add created_at to items with #[]= if already there' do
     yesterday = Time.now.to_i - 86400
-    ResqueMock.push('queue', {:created_at => yesterday})
+    ResqueMock.push('queue', {'created_at' => yesterday})
     existing = ResqueMock.pop('queue')
-    assert_equal yesterday, existing[:created_at]
+    assert_equal yesterday, existing['created_at']
   end
 
   should 'not modify items missing #[]=' do
