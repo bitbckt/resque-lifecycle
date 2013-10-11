e6ddd114114355699bf7358e574821978614a869
grosser <grosser.michael@gmail.com>
2013-10-11 10:52:31 -0700

    fix #2

--- test/helper.rb
+++ test/helper.rb
@@ -6,9 +6,20 @@ require 'resque-lifecycle'
 
 module ResqueMock
   extend self
+  @items = {}
+
+  def clear
+    @items = {}
+  end
 
   def push(queue, item)
+    @items[queue] ||= []
+    @items[queue].push(item)
     item
   end
-end
 
+  def pop(queue)
+    @items[queue] ||= []
+    @items[queue].pop
+  end
+end
--- test/test_lifecycle.rb
+++ test/test_lifecycle.rb
@@ -5,12 +5,23 @@ module ResqueMock
 end
 
 describe Resque::Lifecycle do
+  before do
+    ResqueMock.clear
+  end
+
   it 'adds created_at to items with #[]=' do
-    ResqueMock.push('queue', { })[:created_at].wont_equal nil
+    ResqueMock.push('queue', { })["created_at"].wont_equal nil
   end
 
   it 'does not modify items missing #[]=' do
     item = Object.new
     ResqueMock.push('queue', item).object_id.must_equal item.object_id
+  end
+
+  it 'does not add created_at to items with #[]= if already there' do
+    yesterday = Time.now.to_i - 86400
+    ResqueMock.push('queue', {'created_at' => yesterday})
+    existing = ResqueMock.pop('queue')
+    existing['created_at'].must_equal yesterday
   end
 end
